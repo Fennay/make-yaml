2d80deaa14c637f74ae82e74a4bc6d4459fe0beb
Fennay <feng@fennay.com>
2017-11-13 20:22:45 +0800

    copy

--- index.php
+++ index.php
@@ -11,15 +11,56 @@ class MakeYaml
 
     protected $data;
     protected $fileName;
+    protected $getData;
 
     public function __construct($getData, $fileName = 'test.yaml')
     {
+        $this->getData = $getData;
         $data = [];
         $data['swagger'] = '2.0';
-        $data['info'] = $getData['info'];
+        $data['info'] = $this->info();
+        $data['host'] = $this->host();
+        $data['host'] = $this->get('basePath');
+        $data['host'] = $this->get('schemes');
         $data['paths'] = $this->handlePath($getData['path']);
         $this->data = $data;
         $this->fileName = $fileName;
+
+    }
+
+    protected function info()
+    {
+        $license = [
+            'MIT'       => 'https://opensource.org/licenses/MIT',
+            'Apache2.0' => 'https://opensource.org/licenses/Apache-2.0',
+            'BSD3'      => 'https://opensource.org/licenses/BSD-3-Clause',
+            'BSD2'      => 'https://opensource.org/licenses/BSD-2-Clause',
+            'GPL'       => 'https://opensource.org/licenses/gpl-license',
+            'Mozilla'   => 'https://opensource.org/licenses/MPL-2.0',
+            'LGPL'      => 'https://opensource.org/licenses/lgpl-license',
+        ];
+        $info = $this->getData['info'];
+        $info['license']['url'] = $license[$info['license']['name']];
+
+        return $info;
+    }
+
+    protected function host()
+    {
+        $host = $this->getData['host'];
+        $prefix = 'http://';
+        $return = !empty($host) && (false === stripos($host, $prefix))
+            ?
+            $return = $prefix . $host
+            :
+            '';
+
+        return $return;
+    }
+
+    protected function get($key)
+    {
+        return $this->getData[$key];
     }
 
     public function run()
@@ -65,29 +106,9 @@ class MakeYaml
 
     protected function licenses()
     {
-        $arr = [
-            'MIT' => 'https://opensource.org/licenses/MIT',
-            'Apache2.0' => 'https://opensource.org/licenses/Apache-2.0',
-            'BSD3' => 'https://opensource.org/licenses/BSD-3-Clause',
-            'BSD2' => 'https://opensource.org/licenses/BSD-2-Clause',
-            'GPL' => 'https://opensource.org/licenses/gpl-license',
-            'Mozilla' => 'https://opensource.org/licenses/MPL-2.0',
-            'LGPL' => 'https://opensource.org/licenses/lgpl-license',
-        ];
+
     }
 
-    /**
-     * @param $fileName
-     * @param $data
-     * @author: Mikey
-     */
-    public function display($fileName, $data)
-    {
-        $base = 'view/';
-        $fileName = empty($fileName) ? 'index.html' : $fileName;
-        $html = file_get_contents($base . $fileName);
-        echo $html;
-    }
 }
 
 // $get = $_GET;
@@ -101,3 +122,4 @@ class MakeYaml
 $name = 'test.yaml';
 $makeYaml = new MakeYaml($_GET, $name);
 $makeYaml->run();
+
